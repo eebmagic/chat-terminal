3588b1b1e1e2767e6f26ead64006d976dfdb3adc
Everett Bolton <everett.bolton@itential.com>
2024-01-10 14:56:20 -0500

    migration to updated openai interface

--- chat_gpt_terminal.py
+++ chat_gpt_terminal.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python3
 import os
 import sys
-import openai
+from openai import OpenAI
 from dotenv import load_dotenv
 load_dotenv()
 
 key = os.environ.get('OPENAI_TOKEN')
 if not key:
     quit("OPENAI TOKEN NOT FOUND: Add it into the .env file for the chat-terminal project")
-openai.api_key = key
+
+client = OpenAI(api_key=key)
 
 steering_prompt = "Write a macos terminal command to perform the user's request. Return only the text that should be directly written into the user's terminal session. If no feasible macos command is possible then return the phrase `NO COMMAND FOUND`."
 
@@ -19,26 +20,25 @@ if not query:
     except KeyboardInterrupt:
         quit()
 
-response = openai.ChatCompletion.create(
-  model="gpt-4",
-  messages=[
-    {
-        "role": "system",
-        "content": steering_prompt
-    },
-    {
-        "role": "user",
-        "content": query
-    }
-  ],
-  temperature=1.0,
-  max_tokens=75,
-  top_p=1,
-  frequency_penalty=0,
-  presence_penalty=0
+response = client.chat.completions.create(model="gpt-4",
+    messages=[
+        {
+            "role": "system",
+            "content": steering_prompt
+        },
+        {
+            "role": "user",
+            "content": query
+        }
+    ],
+    temperature=1.0,
+    max_tokens=75,
+    top_p=1,
+    frequency_penalty=0,
+    presence_penalty=0
 )
 
-subresp = response['choices'][0]['message']['content']
+subresp = response.choices[0].message.content
 print(f"\n  {subresp}")
 
 cont = input(f'\nRun this command? [Y/n]: ').strip()
